7d32ff3d76bda4d30ae4fcb4f9cd4fd0269486b5
hannut91 <hannut91@gmail.com>
2021-10-13 23:48:18 +0900

    Define Discount domain model
    유비쿼터스 언어를 보고 할인 도메인 모델을 만들었습니다.
    할인 도메인 모델은 할인이 할인 대상 상품을 정할 수 있습니다.

---
+++ src/main/java/com/kurly/promotion/domain/Discount.java
@@ -0,0 +1,37 @@
+package com.kurly.promotion.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Discount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Integer flatRate;
+
+    private Long productId;
+
+    private Discount(Integer flatRate) {
+        this.flatRate = flatRate;
+    }
+
+    public static Discount createFlatRateDiscount(Integer flatRate) {
+        return new Discount(flatRate);
+    }
+
+    public void attach(Long productId) {
+        this.productId = productId;
+    }
+}
