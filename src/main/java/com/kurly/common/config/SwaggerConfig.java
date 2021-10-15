20b6fa28eb92cd30d0a2c2eea04816bac78b5741
etff <wish2rich@gmail.com>
2021-10-15 11:37:34 +0900

    add swagger config
    - api 문서화를 위한 도구로 Swagger를 의존성에 추가했습니다.

---
+++ src/main/java/com/kurly/common/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.kurly.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    private static final String BASE_PACKAGE = "com.kurly";
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("PROMOTION")
+                .version("1.0.0")
+                .build();
+    }
+}
