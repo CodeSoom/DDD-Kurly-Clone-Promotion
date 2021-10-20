a44a865e64257de0d5d0c7f9bf29caf8ee9999a6
etff <wish2rich@gmail.com>
2021-10-20 20:03:00 +0900

    기간을 추가하라

---
+++ src/test/java/com/kurly/coupon/domain/PeriodTest.java
@@ -0,0 +1,47 @@
+package com.kurly.coupon.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+class PeriodTest {
+
+    @DisplayName("시작일과 종료일로 기간을 생성할 수 있다.")
+    @Test
+    void create() {
+        // given
+        final LocalDate startDate = LocalDate.of(2030, 1, 1);
+        final LocalDate endDate = LocalDate.of(2030, 12, 31);
+        final Period period = Period.createPeriod(startDate, endDate);
+
+        // then
+        assertThat(period).isNotNull();
+    }
+
+    @DisplayName("시작일이 종료일보다 이후면 예외를 던진다.")
+    @Test
+    void start_date_after_end_date() {
+        // given
+        final LocalDate startDate = LocalDate.of(2050, 1, 1);
+        final LocalDate endDate = LocalDate.of(2030, 12, 31);
+
+        assertThatCode(() -> Period.createPeriod(startDate, endDate))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("종료일이 시작일 이전이면 예외를 던진다.")
+    @Test
+    void end_date_before_start_date() {
+        // given
+        final LocalDate startDate = LocalDate.of(2030, 1, 1);
+        final LocalDate endDate = LocalDate.of(2000, 12, 31);
+
+
+        assertThatCode(() -> Period.createPeriod(startDate, endDate))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
