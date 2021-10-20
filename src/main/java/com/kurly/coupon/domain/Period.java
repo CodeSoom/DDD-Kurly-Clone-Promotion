4d3a363bf8c7e3e63de12dc2b4160b13eb823199
etff <wish2rich@gmail.com>
2021-10-20 20:03:00 +0900

    기간을 추가하라

---
+++ src/main/java/com/kurly/coupon/domain/Period.java
@@ -0,0 +1,64 @@
+package com.kurly.coupon.domain;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Embeddable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * 기간.
+ */
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Embeddable
+public class Period {
+    private static final String START_DATE_AFTER_END_DATE_EXCEPTION = "시작일보다 종료일이 이후가 될 수 없습니다.";
+    private static final String END_DATE_BEFORE_START_DATE_EXCEPTION = "종료일이 시작일보다 먼저 올 수 없습니다.";
+
+    /**
+     * 시작일.
+     */
+    private LocalDate startDate;
+
+    /**
+     * 만료일.
+     */
+    private LocalDate endDate;
+
+    private Period(LocalDate startDate, LocalDate endDate) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public static Period createPeriod(LocalDate startDate, LocalDate endDate) {
+        if (startDate.isAfter(endDate)) {
+            throw new IllegalArgumentException(START_DATE_AFTER_END_DATE_EXCEPTION);
+        }
+        if (endDate.isBefore(startDate)) {
+            throw new IllegalArgumentException(END_DATE_BEFORE_START_DATE_EXCEPTION);
+        }
+        return new Period(startDate, endDate);
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Period)) return false;
+        Period period = (Period) o;
+        return Objects.equals(getStartDate(), period.getStartDate()) && Objects.equals(getEndDate(), period.getEndDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getStartDate(), getEndDate());
+    }
+}
