a11b3026265d45aacc059a5302ab25c66846ea80
etff <wish2rich@gmail.com>
2021-10-21 21:05:36 +0900

    동일성 비교 기능을 추가한다

--- src/main/java/com/kurly/coupon/domain/Amount.java
+++ src/main/java/com/kurly/coupon/domain/Amount.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -25,5 +26,18 @@ public class Amount {
             throw new IllegalArgumentException(MINIMUM_NUMBER_REQUIRED);
         }
         return new Amount(value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Amount)) return false;
+        Amount amount = (Amount) o;
+        return Objects.equals(getValue(), amount.getValue());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getValue());
     }
 }
