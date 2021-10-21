ee8db0dd7dd868d82f9fa4637a900e48378ff0d8
etff <wish2rich@gmail.com>
2021-10-22 00:53:52 +0900

    유효성 검사를 추가하라

--- src/main/java/com/kurly/coupon/infra/CouponPolicyRepository.java
+++ src/main/java/com/kurly/coupon/infra/CouponPolicyRepository.java
@@ -1,7 +1,13 @@
 package com.kurly.coupon.infra;
 
 import com.kurly.coupon.domain.CouponPolicy;
+import com.kurly.coupon.domain.Keyword;
+import com.kurly.coupon.domain.Name;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CouponPolicyRepository extends JpaRepository<CouponPolicy, Long> {
+
+    List<CouponPolicy> findByNameOrKeyword(Name name, Keyword keyword);
 }
