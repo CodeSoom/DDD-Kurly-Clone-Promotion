34f4d8e46d6b9465022b195a37bae9b50b193eb0
etff <wish2rich@gmail.com>
2021-10-22 11:48:06 +0900

    오타를 수정하라

--- src/main/java/com/kurly/coupon/application/factory/FlatRatePolicyFactory.java
+++ src/main/java/com/kurly/coupon/application/factory/FlatRatePolicyFactory.java
@@ -1,6 +1,12 @@
 package com.kurly.coupon.application.factory;
 
-import com.kurly.coupon.domain.*;
+import com.kurly.coupon.domain.Count;
+import com.kurly.coupon.domain.CouponPolicy;
+import com.kurly.coupon.domain.Keyword;
+import com.kurly.coupon.domain.Name;
+import com.kurly.coupon.domain.Period;
+import com.kurly.coupon.domain.PolicyType;
+import com.kurly.coupon.domain.Rate;
 import com.kurly.coupon.dto.CouponPolicyPublishData;
 
 import java.util.Objects;
@@ -14,12 +20,12 @@ public class FlatRatePolicyFactory implements PolicyFactory {
 
     @Override
     public CouponPolicy publishPolicy(CouponPolicyPublishData dto) {
-        final Keyword generatedNumber = Keyword.valueOf(dto.getKeyword());
+        final Keyword keyword = Keyword.valueOf(dto.getKeyword());
         final Period period = Period.createPeriod(dto.getStartDate(), dto.getEndDate());
         final Count count = Count.valueOf(dto.getCount());
         final Rate rate = Rate.valueOf(dto.getRate());
         final Name name = Name.valueOf(dto.getName());
 
-        return CouponPolicy.publishRatePolicy(name, generatedNumber, period, rate, count);
+        return CouponPolicy.publishRatePolicy(name, keyword, period, rate, count);
     }
 }
