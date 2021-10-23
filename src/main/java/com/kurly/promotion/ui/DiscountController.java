2f976526aa0df21fd16aebb6b4810f6d9f4edb6c
hannut91 <hannut91@gmail.com>
2021-10-23 18:25:54 +0900

    분리된 컨트롤러를 하나로 합쳐라
    종명님이 만드신 `com.kurly.promotion.controller`와
    `com.kurly.promotion.ui`가 두개로 나뉘어져서 `com.kurly.promotion.ui`로
    합쳤습니다.

--- src/main/java/com/kurly/promotion/ui/DiscountController.java
+++ src/main/java/com/kurly/promotion/ui/DiscountController.java
@@ -1,21 +1,39 @@
 package com.kurly.promotion.ui;
 
 import com.kurly.promotion.application.DiscountService;
-import com.kurly.promotion.domain.Discount;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kurly.promotion.dto.DiscountRegistrationData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
+/**
+ * 할인과 관련된 HTTP 요청 처리를 담당합니다.
+ */
+@RequiredArgsConstructor
 @RequestMapping("/discounts")
 @RestController
 public class DiscountController {
 
     private final DiscountService discountService;
 
-    public DiscountController(DiscountService discountService) {
-        this.discountService = discountService;
+    /**
+     * 주어진 할인 정보를 등록합니다.
+     *
+     * @param discountRegistrationData 할인 등록 정보
+     * @return 응답 정보
+     */
+    @PostMapping
+    public ResponseEntity<Void> register(
+            @RequestBody @Valid DiscountRegistrationData discountRegistrationData
+    ) {
+        Long registeredId = discountService.registerDiscount(
+                discountRegistrationData.getFlatRate(), discountRegistrationData.getProductId()
+        );
+        return ResponseEntity.created(URI.create("/discounts/" + registeredId)).build();
     }
 
     @GetMapping
--- src/test/java/com/kurly/promotion/ui/DiscountControllerTest.java
+++ src/test/java/com/kurly/promotion/ui/DiscountControllerTest.java
@@ -1,7 +1,9 @@
 package com.kurly.promotion.ui;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kurly.promotion.application.DiscountService;
 import com.kurly.promotion.domain.Discount;
+import com.kurly.promotion.dto.DiscountRegistrationData;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -9,31 +11,72 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(DiscountController.class)
 class DiscountControllerTest {
 
-    private List<Discount> discountList;
-
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private DiscountService discountService;
 
+    private DiscountRegistrationData discountRegistrationData;
+    private DiscountRegistrationData invalidDiscountRegistrationData;
+
+    private final Long registeredId = 1L;
+
+    private List<Discount> discountList;
+
     @BeforeEach
     void setUp() {
+        discountRegistrationData = DiscountRegistrationData.builder()
+                .productId(1L)
+                .flatRate(25)
+                .build();
+
+        invalidDiscountRegistrationData = DiscountRegistrationData.builder()
+                .productId(1L)
+                .build();
+
         this.discountList = List.of(
                 new Discount(1L, 10, 1004L)
         );
+    }
+
+    @DisplayName("POST 요청은 올바른 할인 정보가 주어진다면 상태코드 201 Created 를 응답한다.")
+    @Test
+    void registerWithValidDiscountRegisterData() throws Exception {
+        given(discountService.registerDiscount(any(Integer.class), any(Long.class)))
+                .willReturn(registeredId);
+
+        mockMvc.perform(post("/discounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(discountRegistrationData)))
+                .andExpect(header().string("location", "/discounts/" + registeredId))
+                .andExpect(status().isCreated());
+    }
+
+    @DisplayName("POST 요청은 올바르지 않은 할인 정보가 주어진다면 상태코드 400 BadRequest 를 응답한다.")
+    @Test
+    void registerWithInValidDiscountRegisterData() throws Exception {
+        mockMvc.perform(post("/discounts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(invalidDiscountRegistrationData)))
+                .andExpect(status().isBadRequest());
     }
 
     @DisplayName("GET /discounts")
